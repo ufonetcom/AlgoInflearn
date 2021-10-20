cbffad16dd66986c0b88d89c96de6bb4dd9e29f6
ufonetcom <ufozx@naver.com>
2021-10-20 17:51:10 +0900

    1,2번문제

--- src/string/Two.java
+++ src/string/Two.java
@@ -7,20 +7,20 @@ public class Two {
 
     public int solution(String str, char t) {
         int answer = 0;
-        
+
 
         return answer;
     }
 
     public static void main(String[] args){
-        Two one_one = new Two();
+        Two two = new Two();
         Scanner kb = new Scanner(System.in);
 
         String str = kb.next();
         char c = kb.next().charAt(0);
 
-        System.out.println(one_one.solution(str, c));
-        return ;
+        System.out.println(two.solution(str, c));
+
     }
 }
 
