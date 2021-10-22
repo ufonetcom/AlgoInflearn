4796eb8f89b5245241017270a86b33302ea09a04
ufonetcom <ufozx@naver.com>
2021-10-22 17:31:55 +0900

    6번문제 - 중복 문자 제거

--- src/string/Six.java
+++ src/string/Six.java
@@ -8,7 +8,7 @@ public class Six {
     public String solution(String str) {
         String answer="";
         for (int i = 0; i < str.length(); i++) {
-            if(str.indexOf(str.charAt(i))==i){ //indexOf(str) str이 처음 발견된 인덱스 반환, i는 현재 실제 인덱스
+            if(str.indexOf(str.charAt(i))==i){ //indexOf(str) str이 처음 발견된 인덱스 반환, i는 현재 실제 인덱스.
                 answer += str.charAt(i);
             }
         }
