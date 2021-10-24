e6561746174ff95a921427c8641c6f86edf22a04
ufonetcom <ufozx@naver.com>
2021-10-24 13:35:13 +0900

    10번문제 - 가장 짧은 문자거리(완)

--- src/string/StringDistanceShortest.java
+++ src/string/StringDistanceShortest.java
@@ -4,13 +4,30 @@ import java.util.Scanner;
 
 public class StringDistanceShortest {
 
-    public String solution(String str, String x) {
-        String answer="";
-        char[] s = str.toCharArray();
-        char ss = x.charAt(0);
-        for(char xx : s){
-            if(xx == ss){
+    public int[] solution(String str, char t) {
+        int[] answer=new int[str.length()];
+        char[] strc = str.toCharArray();
 
+        int max = 1000; //초기 거리를 넉넉하게 잡기.
+        //왼쪽 문자로부터의 거리 계산
+        for(int i=0; i<strc.length; i++){
+            if(strc[i] == t){
+                max = 0;
+                answer[i] = max;
+            }else {
+                max++;
+                answer[i] = max;
+            }
+        }
+
+        max = 1000;
+        //오른쪽 문자로부터의 거리 계산
+        for(int i=strc.length-1; i>=0; i--){
+            if(strc[i] == t){
+                max = 0;
+            }else {
+                max++;
+                answer[i] = Math.min(answer[i], max);
             }
         }
         return answer;
@@ -21,8 +38,10 @@ public class StringDistanceShortest {
         Scanner kb = new Scanner(System.in);
 
         String str=kb.next();
-        String x = kb.next();
-        System.out.println(ten.solution(str, x));
+        char c = kb.next().charAt(0);
+        for(int x : ten.solution(str, c)){
+            System.out.print(x + " ");
+        }
 
     }
 }
