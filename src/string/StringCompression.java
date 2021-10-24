23c6d1626c58452d578dfe49f0cb12e38046b865
ufonetcom <ufozx@naver.com>
2021-10-24 15:03:59 +0900

    11번문제 - 문자열 압축(나의풀이및 다른풀이)

--- src/string/StringCompression.java
+++ src/string/StringCompression.java
@@ -6,23 +6,36 @@ public class StringCompression {
 
     public String solution(String str) {
         String answer="";
-        char[] s = str.toCharArray();
-        int tmp = 0;
-        char temp = str.charAt(0);
-        for (int i = 0; i < str.length(); i++) {
-            if(temp == s[i]){
-                tmp++;
-                if(i == str.length()-1){
-                    answer += temp + String.valueOf(tmp);
-                }
+        str = str + " ";
+        int cnt = 1;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if(str.charAt(i) == str.charAt(i+1)){
+                cnt++;
             }else {
-                answer += temp;
-                if(tmp > 1) answer += String.valueOf(tmp); //문자열을 숫자로.. Integer.toString(), int + ""
-                tmp = 1;
-                temp = s[i];
+                answer += str.charAt(i);
+                if(cnt > 1) answer += String.valueOf(cnt);
+                cnt = 1;
             }
         }
-        if(tmp == 1) answer += temp;
+
+
+//        char[] s = str.toCharArray();
+//        int tmp = 0;
+//        char temp = str.charAt(0);
+//        for (int i = 0; i < str.length(); i++) {
+//            if(temp == s[i]){
+//                tmp++;
+//                if(i == str.length()-1){
+//                    answer += temp + String.valueOf(tmp);
+//                }
+//            }else {
+//                answer += temp;
+//                if(tmp > 1) answer += String.valueOf(tmp); //문자열을 숫자로.. Integer.toString(), int + ""
+//                tmp = 1;
+//                temp = s[i];
+//            }
+//        }
+//        if(tmp == 1) answer += temp;
         return answer;
     }
 
