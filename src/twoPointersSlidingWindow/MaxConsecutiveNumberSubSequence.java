1f0185f93c5a93279b8167e3bd4b48fc6c6933f6
ufonetcom <ufozx@naver.com>
2021-11-05 22:31:12 +0900

    Two pointers, Sliding window[효율성 : O(n^2)-->O(n)]_5,6번문제 - 최대길이 연속부분수열(다른풀이)

--- src/twoPointersSlidingWindow/MaxConsecutiveNumberSubSequence.java
+++ src/twoPointersSlidingWindow/MaxConsecutiveNumberSubSequence.java
@@ -3,34 +3,21 @@ package twoPointersSlidingWindow;
 import java.util.Scanner;
 
 public class MaxConsecutiveNumberSubSequence {
-    public int solution(int n, int m, int[] arr) {
-        int answer = 0;
-        int zerot = 0;
-        int length = 0;
-        for (int i = 0; i < n; i++) {
-            int k = m;
-            length = i;
-            if(arr[i] == 0){
-                System.out.println(i+1+"번째 0일때");
-                zerot=i;
-                while (k>-1){
-                    if(arr[zerot]==0) k--;
-                    zerot++;
-                    if(zerot==n) break;
-
-                    length++;
-                }
-                System.out.println("length : "+length);
-                System.out.println("i : "+i);
-                System.out.println("zero : "+zerot);
-
-                length = length-i;
-                if(length>answer) answer = length;
-                System.out.println("length : "+length);
-                System.out.println("answer : "+answer);
-                System.out.println();
+    public int solution(int n, int k, int[] arr) {
+        int answer = 0, lt = 0;
+        int cnt = 0;
+        for (int rt = 0; rt < n; rt++) {
+            if(arr[rt] == 0){
+                //0을 1로 바꾸고 cnt를 1증가(0을1로바꿧을때 증가변수)
+                cnt++;
             }
-
+            //0을 1로 바꾼 횟수 cnt가 k를 넘어간다면
+            while (cnt>k){
+                //lt는 rt방향으로 따라가면서 0을 만나면 하나 지워주고 다시 rt움직이기.
+                if(arr[lt]==0) cnt--;
+                lt++;
+            }
+            answer = Math.max(answer, rt-lt+1);
         }
 
         return answer;
