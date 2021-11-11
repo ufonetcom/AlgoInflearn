f4f2d4b968064ca055ef6a56caf7c46f36c5cf26
ufonetcom <ufozx@naver.com>
2021-11-11 21:08:01 +0900

    Stack,Queue (자료구조) 2번문제 : 괄호문자제거

--- src/stackAndQueue/RemoveToBracketCharacter.java
+++ src/stackAndQueue/RemoveToBracketCharacter.java
@@ -7,6 +7,15 @@ public class RemoveToBracketCharacter {
     public String solution(String n) {
         String answer = "";
         Stack<Character> stack = new Stack<>();
+        for(char x : n.toCharArray()){
+            if(x==')' && stack.contains('(')){
+                while (stack.pop() != '('); //"(" 까지 꺼내고 이 "("값을 반환한다.
+            }else if(x!=')') stack.push(x);
+        }
+        for(char x : stack){
+            //stack은 배열을 세워놓은 구조이기 때문에 stack.get(i)의 값을 출력할 수 있다.
+            answer = answer+x;
+        }
 
         return answer;
     }
