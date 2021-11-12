1428972daf93548ef3534aef7e1deaabda290734
ufonetcom <ufozx@naver.com>
2021-11-13 03:05:17 +0900

    Stack,Queue (자료구조) 4번문제 : 후위식연산

---
+++ src/stackAndQueue/PostfixCalculation.java
@@ -0,0 +1,22 @@
+package stackAndQueue;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class PostfixCalculation {
+    public int solution(String n) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        PostfixCalculation sol = new PostfixCalculation();
+        Scanner kb = new Scanner(System.in);
+
+        String m = kb.nextLine();
+        System.out.print(sol.solution(m));
+
+    }
+}
+
