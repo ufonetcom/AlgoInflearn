8cad631d0906c70050bb7626e2d6f4c96d5d7e46
ufonetcom <ufozx@naver.com>
2021-11-18 19:48:16 +0900

    Sorting and Searching(정렬, 이분검색과 결정알고리즘) : 1.선택정렬

--- src/sortingandSearching/SelectionSort.java
+++ src/sortingandSearching/SelectionSort.java
@@ -1,15 +1,23 @@
 package sortingandSearching;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class SelectionSort {
-    public int[] solution(int n, int arr[]) {
-        int[] answer = new int[n];
+    public int[] solution(int n, int[] arr) {
+        for(int i=0; i<n-1; i++){
+            int idx = i;
+            for(int j=i+1; j<n; j++){
+                if(arr[j]<arr[idx]) idx = j;
+            }
+            int tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
 
-
-        return answer;
+        return arr;
     }
 
     public static void main(String[] args){
@@ -18,10 +26,10 @@ public class SelectionSort {
 
         int n = kb.nextInt();
         int[] arr = new int[n];
-        for(int i : arr) {
+        for(int i=0; i<n; i++) {
             arr[i] = kb.nextInt();
         }
-        System.out.print(sol.solution(n,arr));
+        for(int i : sol.solution(n,arr)) System.out.print(i + " ");
 
     }
 }
