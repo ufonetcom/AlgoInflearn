2c118b3053547c15501b724c5787857ddb03a633
ufonetcom <ufozx@naver.com>
2021-11-24 20:50:16 +0900

    Sorting and Searching(정렬, 이분검색과 결정알고리즘) : 8.이분검색(이진탐색 = BinarySearch)

--- src/sortingandSearching/DecisionTree.java
+++ src/sortingandSearching/DecisionTree.java
@@ -1,29 +1,17 @@
 package sortingandSearching;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class BinarySearch {
+public class DecisionTree {
     public int solution(int n,int m, int[] arr) {
         int answer = 0;
-        //이진탐색은 항상 배열(리스트)이 정렬이 되어있어야 한다.
-        Arrays.sort(arr);
-        int lt=0, rt=n-1;
-        while (lt<=rt){
-            int mid = (lt+rt)/2;
-            if(arr[mid]==m){
-                answer = mid+1;
-                break;
-            }
-            if(arr[mid]>m) rt = mid-1;
-            else lt = mid + 1;
-        }
+
 
         return answer;
     }
 
     public static void main(String[] args){
-        BinarySearch sol = new BinarySearch();
+        DecisionTree sol = new DecisionTree();
         Scanner kb = new Scanner(System.in);
 
         int n = kb.nextInt();
