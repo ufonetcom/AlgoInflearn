6615c3e3f430a3ea0ec51aa84163c06c3ec3b727
ufonetcom <ufozx@naver.com>
2021-11-28 21:43:04 +0900

    Recursive, Tree, Graph(DFS, BFS 기초) : 1.재귀함수(스택프레임)

---
+++ src/recursiveTreeGraphexDFSBFS/Recursivefunction.java
@@ -0,0 +1,24 @@
+package recursiveTreeGraphexDFSBFS;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Recursivefunction {
+    public int solution(int n) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Recursivefunction sol = new Recursivefunction();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+
+        System.out.println(sol.solution(n));
+
+    }
+}
+
