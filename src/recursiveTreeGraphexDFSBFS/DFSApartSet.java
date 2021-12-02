c1342a22d36a34a023a2f9901885064a6d4c2809
ufonetcom <ufozx@naver.com>
2021-12-03 01:22:54 +0900

    Recursive, Tree, Graph(DFS, BFS 기초) : 6. 부분집합 구하기(DFS)

---
+++ src/recursiveTreeGraphexDFSBFS/DFSApartSet.java
@@ -0,0 +1,28 @@
+package recursiveTreeGraphexDFSBFS;
+
+public class DFSApartSet {
+    static int n;
+    static int[] ch;
+    public void DFS(int L) {
+        if(L==n+1){
+            String tmp="";
+            for(int i=1; i<=n; i++){
+                if(ch[i]==1) tmp+=(i+" ");
+            }
+            if(tmp.length()>0) System.out.println(tmp); //공집합(0)이 아니면 출력한다.
+        }else {
+            ch[L]=1;
+            DFS(L+1); //왼쪽(사용한다)
+            ch[L]=0;
+            DFS(L+1); //오른쪽(사용하지 않는다)
+        }
+    }
+
+    public static void main(String[] args){
+        DFSApartSet sol = new DFSApartSet();
+        n = 3;
+        ch = new int[n+1];
+        sol.DFS(1);
+    }
+}
+
