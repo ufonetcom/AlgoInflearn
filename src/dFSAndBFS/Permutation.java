5d434a0f89d5947a7439521bbfe70076b279013d
ufonetcom <ufozx@naver.com>
2021-12-15 23:12:21 +0900

    DFS, BFS 활용 : 6. 순열 구하기(채점지원안됨)

--- src/dFSAndBFS/Permutation.java
+++ src/dFSAndBFS/Permutation.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 //순열구하기(중복x)
 //어떤 경우의 수들을 따질때는 DFS를 사용하시고, 최단거리, 최소거리를 구할 때는 BFS라는 사실을 염두에 두고 알고리즘 공부.
 public class Permutation {
-    static int[] pm, ch, arr;
     static int n,m;
     public void DFS(int L) {
 
